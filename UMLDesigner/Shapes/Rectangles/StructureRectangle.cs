03a80f4d925b0a8b812654af75624a87838a9629
creatorQuen <spastime@yandex.ru>
2021-04-26 01:20:05 +0300

    Change StructureRectangle when write text

--- UMLDesigner/Shapes/Rectangles/StructureRectangle.cs
+++ UMLDesigner/Shapes/Rectangles/StructureRectangle.cs
@@ -16,8 +16,6 @@ namespace UMLDesigner.Shapes.Rectangles
         private int _methodsHeight;
         private int _deltaX = 0;
 
-
-        AbstractRectangle _absR;
         public Font nameFont = new Font("Arial", 18);
         Graphics graphics = MyGraphics.GetInstance().GetMainGraphics();
 
@@ -30,70 +28,49 @@ namespace UMLDesigner.Shapes.Rectangles
             Pen pen = new Pen(color, penWidth);
 
             _nameHeight = 30 + ((int)graphics.MeasureString(name, nameFont).Width / size.X) * ((int)graphics.MeasureString(name, nameFont).Height + 3);
-            _propertiesHeight = 30 + ((int)graphics.MeasureString(properties, argumentFont).Width / size.X) * ((int)graphics.MeasureString(properties, argumentFont).Height + 3);
+            _propertiesHeight = 30 + ((int)graphics.MeasureString(properties, argumentFont).Width / ((size.X))) * ((int)graphics.MeasureString(properties, argumentFont).Height + 1);
             _fieldsHeight = 30 + ((int)graphics.MeasureString(fields, argumentFont).Width / size.X) * ((int)graphics.MeasureString(fields, argumentFont).Height + 3);
             _methodsHeight = 90 + ((int)graphics.MeasureString(methods, argumentFont).Width / (size.X)) * ((int)graphics.MeasureString(methods, argumentFont).Height + 3);
 
-            //int count = 0;
-            //for(int i = 0; properties.Length > 0; i++)
-            //{
-            //    if(properties[i] == "\n") ;
-            //    {
-            //        count++;
-            //    }
-
-            //}
-
-            string subString = "\n";
             if (!(properties is null))
             {
-                ////int indexOfSubstring = properties.IndexOf(subString);
-                //for (int i = 0; i < properties.Length; i++)
-                //{
-                //    properties[i] = subString;
-                //}
-
-                int iii = (int)graphics.MeasureString(properties, argumentFont).Width;
-                if (iii > size.X)
+                int strMeasure = (int)graphics.MeasureString(properties, argumentFont).Width;
+                if (strMeasure > size.X)
                 {
-                    size.X = size.X + (int)(iii / 2);
+                    size.X = strMeasure + 1;
                 }
             }
             if (!(fields is null))
             {
-                int iii = (int)graphics.MeasureString(fields, argumentFont).Width;
-                if (iii > size.X)
+                int strMeasure = (int)graphics.MeasureString(fields, argumentFont).Width;
+                if (strMeasure > size.X)
                 {
-                    size.X = size.X + (int)(iii / 2);
+                    //size.X = size.X + (int)(strMeasure / 2);
+                    size.X = strMeasure + 1;
                 }
             }
             if (!(methods is null))
             {
-                int iii = (int)graphics.MeasureString(methods, argumentFont).Width;
-                if (iii > size.X)
+                int strMeasure = (int)graphics.MeasureString(methods, argumentFont).Width;
+                if (strMeasure > size.X)
                 {
-                    size.X = size.X + (int)(iii / 2);
+                    //size.X = size.X + (int)(strMeasure / 2);
+                    size.X = strMeasure + 1;
                 }
             }
 
-
+            int nameWidth = size.X;
             int propWidth = size.X;
             int fieldstWidth = size.X;
             int methodsWidth = size.X;
 
-
-            //int propWidth = size.X + ((int)graphics.MeasureString(properties, argumentFont).Width / size.X) * ((int)graphics.MeasureString(properties, argumentFont).Height + 3);
-            //int fieldstWidth = size.X + ((int)graphics.MeasureString(properties, argumentFont).Width / size.X) * ((int)graphics.MeasureString(properties, argumentFont).Height + 3);
-            //int methodsWidth = size.X + ((int)graphics.MeasureString(properties, argumentFont).Width / size.X) * ((int)graphics.MeasureString(properties, argumentFont).Height + 3);
-
-            Rectangle _nameRect = new Rectangle(startPoint.X, startPoint.Y, size.X, _nameHeight);
+          
+            Rectangle _nameRect = new Rectangle(startPoint.X, startPoint.Y, nameWidth, _nameHeight);
             //Rectangle _propertiesRect = new Rectangle(startPoint.X, startPoint.Y + _nameHeight, propWidth, _propertiesHeight);
             //Rectangle _fieldsRect = new Rectangle(startPoint.X, startPoint.Y + _nameHeight + _propertiesHeight, size.X, _fieldsHeight);
             //Rectangle _methodRect = new Rectangle(startPoint.X, startPoint.Y + _nameHeight + _propertiesHeight + _fieldsHeight, size.X, _methodsHeight);
 
-
             Rectangle _propertiesRect = new Rectangle(startPoint.X, startPoint.Y + _nameHeight, propWidth, _propertiesHeight);
-
             Rectangle _fieldsRect = new Rectangle(startPoint.X, startPoint.Y + _nameHeight + _propertiesHeight, fieldstWidth, _fieldsHeight);
             Rectangle _methodRect = new Rectangle(startPoint.X, startPoint.Y + _nameHeight + _propertiesHeight + _fieldsHeight, methodsWidth, _methodsHeight);
 
@@ -111,10 +88,5 @@ namespace UMLDesigner.Shapes.Rectangles
             TextDrawing.DrawText(_methodRect, argumentFont, color, methods);
 
         }
-
-        //public Point GetNewsizeRectangle()
-        //{
-
-        //}
     }
 }
